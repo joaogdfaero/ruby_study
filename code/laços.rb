5ecbacead695bcf9cf38294a6554727560a7ec7d
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-08 15:56:51 -0300

    continuando exercicios de laço

--- code/laços.rb
+++ code/laços.rb
@@ -1,7 +1,7 @@
 # loop descrecente simples
 valor = 5
-while valor>0
-    puts valor
-    valor -= 1 # equivale a valor = valor -1
+while valor<=10
+    puts "O valor é: " + valor.to_s # converte para string
+    valor += 1 # equivale a valor = valor -1
 end
 
