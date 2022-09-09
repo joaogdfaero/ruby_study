c936c9851eb7b12e93c0a2d402008080118ba56a
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-09 07:40:04 -0300

    parametros nomeados

--- code/métodos.rb
+++ code/métodos.rb
@@ -8,5 +8,11 @@ end
 # é preciso executar o método para ter a saída
 
 puts "Ele ignora o método enquanto ele não é chamado, por isso isso aparece primeiro" 
-
 puts soma(10,5)
+
+
+def soma_com_parametros_nomeados(valor1:,valor2:)
+    soma(valor1,valor2)
+end
+
+puts "Vou somar com parâmetros nomeados"
