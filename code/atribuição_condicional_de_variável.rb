b455ce9ae71cc364087f8437dce9b167ff56aa07
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-09 07:49:28 -0300

    fim do exercicio de atribuicao

--- code/atribuição_condicional_de_variável.rb
+++ code/atribuição_condicional_de_variável.rb
@@ -1,6 +1,8 @@
 # só atribui o valor a uma variável caso algo ocorra
 
 # caso a variável seja nula
-variável = 5
-variável = 3 if !variável.nil
-puts variável
+variavel = nil
+#variavel = 5
+
+variavel ||= 20 # só atribui o valor de 20 se ela não for nula
+puts variavel
