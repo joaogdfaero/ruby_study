7b1c21f9c0b28fa14d185178739aeade78e061a5
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-12 14:41:09 -0300

    finalizando exercio final ruby

--- code/exercicio_final.rb
+++ code/exercicio_final.rb
@@ -1,14 +1,14 @@
 # Verifica se a palavra que o usuário entra é palíndromo (igual trás pra frente)
 puts "Digite uma palavra: "
 palavra = gets
-puts palavra
 
 palavra_invertida = palavra.reverse
 puts palavra_invertida
 
-if palavra == palavra_invertida
+if palavra == palavra_invertida 
     puts "A palavra é um palíndromo"
 else
     puts "A palavra não é um palíndromo"
 end
+
 
---
+++ code/exercicio_final_solucaodio.rb
@@ -0,0 +1,24 @@
+def palindromo?(palavra)
+    palavra = palavra.downcase
+    palavra_reversa = ""
+
+    indice = palavra.length
+
+
+
+    until indice == 0
+        letra = palavra[indice -1]
+        palavra_reversa << letra
+        indice = indice-1
+    end
+
+    if palavra_reversa == palavra
+        return true
+    else
+        return false
+    end
+end
+
+puts palindromo?('ovo')
+puts palindromo?('Ovo')
+puts palindromo?('casa')
