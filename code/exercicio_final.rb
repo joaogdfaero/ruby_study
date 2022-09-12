685820d2b2a49e8ef957f0a016386279b65975be
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-12 14:53:49 -0300

    finalizei meu exercicio final

--- code/exercicio_final.rb
+++ code/exercicio_final.rb
@@ -1,9 +1,8 @@
 # Verifica se a palavra que o usuário entra é palíndromo (igual trás pra frente)
 puts "Digite uma palavra: "
-palavra = gets
+palavra = gets.chomp # tem que usar o chomp pra remover a newline https://stackoverflow.com/questions/6008230/why-does-comparing-strings-in-ruby-always-return-false
 
 palavra_invertida = palavra.reverse
-puts palavra_invertida
 
 if palavra == palavra_invertida 
     puts "A palavra é um palíndromo"
