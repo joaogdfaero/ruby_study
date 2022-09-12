677a87fa899ea78841253b54297422e64d69097d
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-12 14:04:47 -0300

    terminando exercicio final

--- code/exercicio_final.rb
+++ code/exercicio_final.rb
@@ -1,1 +1,15 @@
 # Verifica se a palavra que o usuário entra é palíndromo (igual trás pra frente)
+puts "Digite uma palavra: "
+palavra = gets
+puts palavra
+
+palavra_invertida = palavra.reverse
+puts palavra_invertida
+
+if palavra == palavra_invertida
+    puts "A palavra é um palíndromo"
+else
+    "A palavra não é um palíndromo"
+end
+
+    
