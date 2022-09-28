221b7b447696981d8a0a8ad1eb4aed8655709b43
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-28 14:16:12 -0300

    started roman to integer exercise

---
+++ exercises/roman_to_integer.rb
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/roman-to-integer/
+
+# @param {String} s
+# @return {Integer}
+def roman_to_int(s)
+
+    # creating a hash
+    romans = {"I" => 1,"V" => 5,"X" => 10,"L" => 50,"C" => 100,"D" => 500, "M" => 1000}
+
+    p romans["V"]
+
+    
+end
+
+
+s = "II"
+roman_to_int(s)
