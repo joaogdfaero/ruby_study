68eaca15c5d40c30db9d5b21b80935d005bac622
joaogdfaero <joao.dal@acad.ufsm.br>
2022-10-14 14:55:46 -0300

    started pragmatic programmer reading

--- basics/pragmatic_programmer.rb
+++ basics/pragmatic_programmer.rb
@@ -20,4 +20,11 @@ end
 puts "Danger, Will Robinson" if radiation > 3000
 
 
+# Regular expression
+# For example, the following code fragment writes a message if a string contains the text 'Perl' or 'Python'.
+if line =~ /Perl|Python/
+  puts "Scripting language mentioned: #{line}"
+end
+line.sub(/Perl/, 'Ruby')    # replace first 'Perl' with 'Ruby'
+line.gsub(/Python/, 'Ruby') # replace every 'Python' with 'Ruby'
 
